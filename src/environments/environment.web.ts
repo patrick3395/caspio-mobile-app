a67dc04413d145e285e856c81abd470389936fc2
patrick3395 <patrick3395@users.noreply.github.com>
2025-12-09 22:07:46 -0600

    Update environment.web.ts configuration

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -11,12 +11,12 @@ export const environment = {
   },
   useApiGateway: true,
   
-  // Caspio config (credentials removed for security)
+  // Caspio config (only used internally by backend now)
   caspio: {
     tokenEndpoint: 'https://c2hcf092.caspio.com/oauth/token',
     apiBaseUrl: 'https://c2hcf092.caspio.com/rest/v2',
-    clientId: '',
-    clientSecret: ''
+    clientId: '01ddeb9d873748255f3edeccb5fbfa806695e43ffa5fff4f67',
+    clientSecret: '1d4e3ea85a2247f0929a0a995df66e6be183c463391375ae80'
   },
   googleMapsApiKey: 'AIzaSyCOlOYkj3N8PT_RnoBkVJfy2BSfepqqV3A',
   paypal: {
